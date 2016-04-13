bf51fe9fd005383ddec282595da8ec3cb52125f1
andrepeedosaar <andre190@ut.ee>
2016-04-13 10:30:39 +0300

    aksgölasdp
    ölajdrpgpdn

--- header.php
+++ header.php
@@ -4,4 +4,5 @@
     <style type="text/css">@import "./css/defaultstyle.css";</style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="/js/main.js"></script>
+	<script src="/js/typeahead.min.js"></script>
 </head>
