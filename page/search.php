aa532ee087b2f08894c846308c0f95c921f0e851
andrepeedosaar <andre190@ut.ee>
2016-04-13 11:03:51 +0300

    kpknn

--- page/search.php
+++ page/search.php
@@ -10,7 +10,6 @@
 	$key = $_GET["key"];
 	$array = array();
 	$result = pg_query($db, "SELECT firstname, lastname FROM candidate WHERE firstname LIKE '%{key}%'");
-	echo "Tekst";
 	
 	while($row = pg_fetch_assoc($result)) {
 		$array[] = $row["firstname"]." ".$row["lastname"];
