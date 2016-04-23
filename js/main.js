1a5e9a5c8134b3c385b957dee00d7f5e130c0f8d
andrepeedosaar <andre190@ut.ee>
2016-04-23 13:56:18 +0300

    asger
    drgdr

--- js/main.js
+++ js/main.js
@@ -45,10 +45,37 @@
 			source: "search.php",
 			minLength: 2,
 		});
+		
+		$('.historyAPI').on('click', function(e){
+            e.preventDefault();
+            var href = $(this).attr('href');
+             
+            // Getting Content
+            getContent(href, true);
+             
+            jQuery('.historyAPI').removeClass('active');
+            $(this).addClass('active');
+        });
 	
+	// Adding popstate event listener to handle browser back button  
+    window.addEventListener("popstate", function(e) {
+         
+        // Get State value using e.state
+        getContent(location.pathname, false);
+    });
 	
-	
-	
+	function getContent(url, addEntry) {
+        $.get(url).done(function( data ) {
+             
+            // Updating Content on Page
+            $('#contentHolder').html(data);
+             
+            if(addEntry == true) {
+                // Add History Entry using pushState
+                history.pushState(null, null, url); 
+            }
+             
+        });
 	
 	
 	
